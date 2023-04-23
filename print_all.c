a9e98fe9fe3ff0c5782afa66775ad8308e24d4d6
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-04-23 07:35:55 -0700

    1err

--- print_all.c
+++ print_all.c
@@ -4,36 +4,44 @@
 #include <unistd.h>
 /**
  * _printf - fonctiopn to printf
- *_print_all - print all fcs
+ *_print_all- print all fcs
  * @format : the format
  *@args : parametr
  *Return: Printed chars.
  */
-int _print_all(const char *format, va_list args) {
-    int printLen = 0;
-    for (; *format; ++format) {
-        if (*format == '%') {
-            switch (*++format) {
-                case 'c':
-                    printLen += my_write(va_arg(args, int));
-                    break;
-                case 's': {
-                    const char *str = va_arg(args, const char *);
-                    if (!str) return -1;
-                    while (*str) printLen += my_write(*str++);
-                    }
-                    break;
-                case '%':
-                    printLen += my_write('%');
-                    break;
-                default:
-                    return -1;
-            }
-        } else {
-            printLen += my_write(*format);
-        }
-    }
-    va_end(args);
-    return printLen;
+int _print_all(const char *format, va_list args)
+{
+int printLen = 0;
+for (; *format; ++format)
+{
+if (*format == '%')
+{
+switch (*++format)
+{
+case 'c':
+	printLen += my_write(va_arg(args, int));
+break;
+case 's':
+{
+const char *str = va_arg(args, const char *);
+if (!str)
+return (-1);
+while (*str)
+	printLen += my_write(*str++);
 }
-
+break;
+case '%':
+printLen += my_write('%');
+break;
+default:
+return (-1);
+}
+}
+else
+{
+printLen += my_write(*format);
+}
+}
+va_end(args);
+return (printLen);
+}
