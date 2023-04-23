3dfd9622a3849e24cd778f2962a240be3f1c1b9d
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-04-23 14:42:54 -0700

    all

--- main.h
+++ main.h
@@ -1,15 +1,9 @@
 #ifndef main_h
 #define main_h
-int _printf(const char *format, ...);
-int mywriter(char c);
+int my_write(char c);
 int _print_all(const char *format, va_list args);
-
-
-
-
-
-
-
-
+int _printf(const char *format, ...);
+int _printf(const char *format, ...);
+int write_string(char *str);
 
 #endif
