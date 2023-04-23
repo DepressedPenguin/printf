a5bf4cfd95880ceb085fdb7828960cd9912ed344
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-04-23 05:52:58 -0700

    putchar lookalike

--- my_write.c
+++ my_write.c
@@ -1,4 +1,9 @@
-
-int my_write(char c){
-    return (write(1, &c, 1));
+/**
+ *my_write - fonction to push
+ *@c: char
+ *Return: Number of chars printed.
+ */
+int my_write(char c)
+{
+return (write(1, &c, 1));
 }
