a82030413f3be469f5ea1a2b09d32b6cb7690abb
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-04-24 12:33:11 -0700

    khdm

--- _printf.c
+++ _printf.c
@@ -14,47 +14,44 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    char *str;
-    int lenPrint = 0, i;
+va_list args;
+char *str;
+int lenPrint = 0, i;
+if (format == NULL)
+return (-1);
+va_start(args, format);
+for (i = 0; format[i] != '\0'; i++)
+{
+if (format[i] == '%')
+{
+i++;
+switch (format[i])
+{
+case 'c':
+	lenPrint += my_write(va_arg(args, int));
+break;
+case 's':
+str = va_arg(args, char *);
+if (str == NULL)
+lenPrint += write_string("(null)");
+else
+lenPrint += write_string(str);
+break;
+case '%':
+lenPrint += my_write('%');
+break;
+default:
+lenPrint += my_write('%');
+lenPrint += my_write(format[i]);
+break;
+}
+}
+else
+lenPrint += my_write(format[i]);
+}
 
-    if (format == NULL)
-        return (-1);
+va_end(args);
 
-    va_start(args, format);
-
-    for (i = 0; format[i] != '\0'; i++)
-    {
-        if (format[i] == '%')
-        {
-            i++;
-            switch (format[i])
-            {
-                case 'c':
-                    lenPrint += my_write(va_arg(args, int));
-                    break;
-                case 's':
-                    str = va_arg(args, char *);
-                    if (str == NULL)
-                        lenPrint += write_string("(null)");
-                    else
-                        lenPrint += write_string(str);
-                    break;
-                case '%':
-                    lenPrint += my_write('%');
-                    break;
-                default:
-                    
-                    lenPrint += my_write(format[i]);
-                    break;
-            }
-        }
-        else
-            lenPrint += my_write(format[i]);
-    }
-
-    va_end(args);
-
-    return (lenPrint);
+return (lenPrint);
 }
 
