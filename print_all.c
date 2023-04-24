bf75d395e0d3dcac7e23de3211dce6ad2ac27245
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-04-24 09:56:27 -0700

    0

--- print_all.c
+++ print_all.c
@@ -12,33 +12,33 @@
  */
 int _print_all(const char *format, va_list args)
 {
-int i, printLen = 0;
-char *str;
-for (i = 0; format[i] != '\0'; i++)
-{
-if (format[i] == '%')
-{
-i++;
-switch (format[i])
-{
-case 'c':
-printLen += my_write(va_arg(args, int));
-break;
-case 's':
-str = (char *) va_arg(args, char *);
-printLen += write_string(str);
-break;
-case '%':
-printLen += my_write(format[i]);
-break;
-default:
-return (-1);
-}
-}
-else
-{
-printLen += my_write(format[i]);
-}
-}
-return (printLen);
+	int i, lenPrint = 0;
+	char *str;
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			switch (format[i])
+			{
+				case 'c':
+					lenPrint += my_write(va_arg(args, int));
+					break;
+				case 's':
+					str = va_arg(args, char *);
+					lenPrint += write_string(str);
+					break;
+				case '%':
+					lenPrint += my_write('%');
+					break;
+				default:
+					return (-1);
+			}
+		}
+		else
+			lenPrint += my_write(format[i]);
+	}
+
+	return (lenPrint);
 }
