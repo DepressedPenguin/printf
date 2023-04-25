1c527712c368f7a1715f11e902393fa78302e9bd
ZAKARIA <zakariaelaro@gmail.com>
2023-04-25 07:13:48 -0700

    fix errors

--- intm.c
+++ intm.c
@@ -11,6 +11,7 @@ int print_integer_to_buffer(va_list args, char result_buffer[], int flags, int w
 {
 int position = BUFFER_SIZE - 2;
 int is_negative = 0;
+unsigned int num = 0;
 unsigned int num_value = 0;
 
 if (size == sizeof(char))
@@ -29,18 +30,18 @@ else if (size == sizeof(long))
 {
 num_value = va_arg(args, long);
 }
-if (num_value == 0)
+if (num == 0)
 {
 result_buffer[position--] = '0';
-}
+} 
 else 
-{
-if (num_value < 0)
+{    
+if (is_negative)
 {
 is_negative = 1;
-num_value = -num_value;
+num = -num;
 }
-for (long unsigned int num = num_value; num > 0; num /= 10)
+for (num = num_value; num > 0; num /= 10)
 {
 result_buffer[position--] = (num % 10) + '0';
 }
@@ -50,7 +51,8 @@ result_buffer[position--] = '-';
 }
 }
 result_buffer[BUFFER_SIZE - 1] = '\0';
-int digits_written = BUFFER_SIZE - position - 2;
+int digits_written;
+digits_written = BUFFER_SIZE - position - 2;
 int result = write_number(result_buffer + position + 1, flags, width, precision, digits_written);
 return result;
 }
